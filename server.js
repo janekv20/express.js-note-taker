4ff0464b98a981d83b1c418311db6acb988a78bf
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-14 21:42:37 -0500

    adding comments and routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+//require express package
 const express = require ('express');
-
+//create the variable for PORT
 const PORT = process.env.PORT || 3001;
-
+//declare app equals to express method
 const app = express ();
+
+app.listen(3001, () => {
+    console.log('Sever now on running on port 3001');
+});
