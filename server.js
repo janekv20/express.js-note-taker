91e193966d08c3eeec3509ccae10893cf82d83c8
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-14 21:47:36 -0500

    use routes added on server.js

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const PORT = process.env.PORT || 3001;
 //declare app equals to express method
 const app = express ();
 
+//import the routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//parse string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse JSON
+app.use(express.json());
+app.use(express.static('public'));
+
+//use routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//create listen to variable PORT
 app.listen(3001, () => {
     console.log('Sever now on running on port 3001');
 });
