b5d4a9daa1fb7b52aa66c10edc4d51a96951fd4d
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-19 23:05:45 -0500

    added another index.html get route

--- routes/HTML/index.js
+++ routes/HTML/index.js
@@ -3,13 +3,17 @@ const path = require('path');
 //import express router 
 const router = require('express').Router();
 
-//2 routes for html notes
+//3 routes for html notes
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
