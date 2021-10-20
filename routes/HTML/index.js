c0bf37713ead85eb3a585d7ffd8aac0438007789
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-19 23:50:40 -0500

    corrected paths

--- routes/HTML/index.js
+++ routes/HTML/index.js
@@ -5,15 +5,15 @@ const router = require('express').Router();
 
 //3 routes for html notes
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
