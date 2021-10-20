8f1ec6c48bbee7a0cbaa70cf0ed4faf8e91e04e9
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-19 22:16:40 -0500

    updated html routes

--- routes/API/index.js
+++ routes/API/index.js
@@ -5,5 +5,11 @@ const router = require('express').Router();
 
 //2 routes for html notes
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+module.exports = router;
